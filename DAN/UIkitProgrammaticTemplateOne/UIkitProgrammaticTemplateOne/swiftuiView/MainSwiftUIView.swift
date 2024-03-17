79d750566527bc9e08f4564965576f047d6b3e7f
DK <kimani.daniel@eclectics.io>
2024-03-17 16:30:41 +0300

    addedd

---
+++ DAN/UIkitProgrammaticTemplateOne/UIkitProgrammaticTemplateOne/swiftuiView/MainSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  MainSwiftUIView.swift
+//  UIkitProgrammaticTemplateTwo
+//
+//  Created by Daniel Kimani on 17/03/2024.
+//
+
+import SwiftUI
+
+struct MainSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .navigationBarTitle("SWIFT UI Two", displayMode: .inline)
+        
+    }
+}
+
+#Preview {
+    MainSwiftUIView()
+}
---
+++ DAN/UIkitProgrammaticTemplateTwo/UIkitProgrammaticTemplateTwo/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  UIkitProgrammaticTemplateTwo
+//
+//  Created by Daniel Kimani on 17/03/2024.
+//
+
+import UIKit
+import SwiftUI
+import Foundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //self.title = "TemplateTwo"
+        setupUI()
+    }
+
+    
+    //MARK: IF YOU WANT TO USE SWIFTUI USE THIS FUNCTION TO ENABLE YOU LOAD SWIFTUI VIEW
+    private  func setupUI() {
+        // Create your SwiftUI root view
+       let rootView = NavigationView{MainSwiftUIView()}.preferredColorScheme(.light)
+       //let rootView = MainSwiftUIView().preferredColorScheme(.light)
+        
+        //let rootView = NavigationView{Step3StartActivityScreen()}.preferredColorScheme(.light).environmentObject(activityTracker)
+        
+        // Set edgesIgnoringSafeArea to .all except .bottom
+        let viewController = UIHostingController(rootView: rootView)
+        viewController.edgesForExtendedLayout = [.top, .left, .right] // Ignore only the top, left, and right safe areas
+        addChild(viewController)
+        view.addSubview(viewController.view)
+        
+        // Set up Auto Layout constraints
+        viewController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            viewController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            viewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            viewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            viewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        viewController.didMove(toParent: self)
+    }
+
+}
+
